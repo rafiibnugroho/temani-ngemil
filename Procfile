682f46bc68d392e889905f09bfd8fc939b4cfe48
pekantigalau <146416537+pekantigalau@users.noreply.github.com>
2025-10-09 00:03:13 +0700

    fix: correct Procfile for Railway deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && php artisan serve --host=0.0.0.0 --port=$PORT
+web: vendor/bin/heroku-php-apache2 public/
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-composer install --no-dev --optimize-autoloader
-php artisan key:generate
-php artisan migrate --force || true
